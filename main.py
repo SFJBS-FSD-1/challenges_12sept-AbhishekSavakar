73d6ab40665d8b95e7d2895c7aa0ea59c0ae1706
AbhishekSavakar <abhisheksavakar1998@gmail.com>
2022-09-09 05:45:03 +0000

    make it better

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def chome():
+    if request.method == "POST":
+        url = "https://api.covid19api.com/summary"
+        response = requests.get(url).json()
+        lists = response.get("Countries")
+        c = request.form["country"]
+        for i in lists:
+            if i["Country"].lower() == c.lower():
+                data = {"name": i["Country"],
+                        "confirmed": i["TotalConfirmed"],
+                        "deaths": i["TotalDeaths"],
+                        'code': 200}
+                return render_template('home.html', data=data)
+        else:
+            data = {'message': 'Please Enter correct country', 'code': 404}
+            return render_template('home.html', data=data)
+    else:
+        return render_template('home.html', data=None)
+
+
+app.run()
